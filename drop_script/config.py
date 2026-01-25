093923b24714fb1b7c5cad9ab1b842ce52e3dc81
ExxML <andywu9509@gmail.com>
2026-01-25 00:16:19 -0800

    Updated comments

--- drop_script/config.py
+++ drop_script/config.py
@@ -9,12 +9,12 @@ class Config():
         self.COMMAND_CHANNEL_IDS = [
             "",
         ]
-        # Enter your drop channels as a list of strings.
+        # Enter your drop channels as a list of strings. You MUST have at least 1 drop channel for every 3 accounts used.
         self.DROP_CHANNEL_IDS = [
             "",
         ]
         # Enter your server activity drop channels as a list of strings (for reacting to drops with the special event emoji during Karuta events). 
-        # Only used if self.SPECIAL_EVENT = True. Leave the list empty to disable grabbing the special event emoji on server activity based drops.
+        # Only used if self.SPECIAL_EVENT = True or self.GRAB_SERVER_POG_CARDS = True for their respective purposes.
         self.SERVER_ACTIVITY_DROP_CHANNEL_IDS = [
             "",
         ]
@@ -29,4 +29,4 @@ class Config():
         self.DROP_SKIP_RATE = 12  # (int) For every drop, there is a 1/self.DROP_SKIP_RATE chance of skipping the drop. Set to -1 if you wish to disable it skipping.
         self.ONLY_GRAB_POG_CARDS = False  # (bool) Determines whether to ONLY grab pog cards (as defined by CardCompanion). IF CARDCOMPANION IS NOT SET UP, SET THIS TO FALSE, or else no cards will be grabbed.
         self.RANDOM_COMMAND_RATE = 480  # (int) Every 2-3 seconds, there is a 1/self.RANDOM_COMMAND_RATE chance of sending a random command.
-        self.SPECIAL_EVENT = False  # (bool) Whether the script will use the token in special_event_token.json to auto-react with the event emoji (if there is one) during Karuta special events.
+        self.SPECIAL_EVENT = False  # (bool) Whether the script will use the token in special_event_token.json to auto-react with the event emoji during Karuta special events.
